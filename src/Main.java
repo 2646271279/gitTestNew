32a501e25ea68b3555407c360925741f3a90f02b
2646271279 <61074292+2646271279@users.noreply.github.com>
2024-03-17 12:39:09 +0800

    Update Main.java
    a == 0

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!2");
-        System.out.println("Hello world!3");
-        System.out.println("Hello world!5");
-        System.out.println("Hello world!6.5");
-        System.out.println("Hello world!6-7.8");
-        System.out.println("Hello world!7");
-        System.out.println("Hello world!8");
-        System.out.println("Hello world!9");
-        System.out.println("Hello world!9.7");
+        int a = 0;
+        if (a == 0) {
+            System.out.println("a = 0!");
+        }
     }
 }
